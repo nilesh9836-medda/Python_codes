f102fda86763717302aa4d9cf05ff1517af684c7
Nilesh Medda <meddanilesh7@gmail.com>
2025-12-07 00:31:59 +0530

    new file:   code8.py

---
+++ codes/basic/code8.py
@@ -0,0 +1,9 @@
+a="Python Codes"
+b=a.split() #splitting the string
+print(b)
+print(b[1]) #printing the splitting string
+
+c="ElonMusk,SteveJobs,BillGates"
+d=c.split(",") #Splitting string based on Delimitter
+print(d)
+print(d[1])
