7bcd410bf7596642d266918b60df0f70f180b2ff
Nilesh Medda <meddanilesh7@gmail.com>
2025-12-07 00:26:43 +0530

    new file:   code6.py

---
+++ codes/basic/code6.py
@@ -0,0 +1,8 @@
+a="python"
+print(a[3]) #identifying the element based on index
+print(a[2:]) #Grab the remaining elements except upto the Index
+print(a[:2]) #Grab the elements upto the Index
+print(a[-1]) #Grab the Last element
+print(a[:-1]) #Grab the elements except last element
+print(a[::2]) #Grab everything with 2 steps
+print(a[::-1]) #Print string backwards
