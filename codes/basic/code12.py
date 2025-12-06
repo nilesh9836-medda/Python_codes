91743ea450a0e5d0d7d774e1c91dca67c5d66f36
Nilesh Medda <meddanilesh7@gmail.com>
2025-12-07 00:42:16 +0530

    new file:   code12.py

---
+++ codes/basic/code12.py
@@ -0,0 +1,13 @@
+a=10
+b=20
+c=30
+if a>b:
+    if a>c:
+        print("a is the greatest number")
+    else:
+        print("c is the greatest number")
+else:
+    if b>c:
+        print("b is the greatest number")
+    else:
+        print("c is the greatest number")
