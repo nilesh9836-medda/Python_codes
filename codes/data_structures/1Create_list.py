8ce1f86339906f7f9c641c6468fe4f2815b83eb8
Nilesh Medda <meddanilesh7@gmail.com>
2025-12-20 23:50:28 +0530

    new file: 1Create_list.py

---
+++ codes/data_structures/1Create_list.py
@@ -0,0 +1,10 @@
+# Create List
+
+a = [1,2,3,4,5]
+b = ["Champ",21,99.5]
+print(a,b)
+print(len(b)) #Length of List
+print(b[0]) #Locate list element based on Index
+print(a[1:]) #print elements except 1st
+print(a[:2]) #Print elements upto 2nd element
+print(a+b) #Concatenate 2 list
