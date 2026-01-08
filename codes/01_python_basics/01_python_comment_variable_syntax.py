b24b952424a2c9df32378e19625c48419d0e32ac
Nilesh Medda <meddanilesh7@gmail.com>
2026-01-08 22:07:11 +0530

    Create 01_python_basics folder & add 01_python_comment_variable_syntax.py file

---
+++ codes/01_python_basics/01_python_comment_variable_syntax.py
@@ -0,0 +1,29 @@
+# Single line comment
+
+x = 10 # Assign the integer vale to 'x'
+y = 15.9856 #Assign the floatin point value to 'y'
+z = 'abcd' # Assign the character value to 'z'
+a = "I\'m coding python" # Assign the string value to 'a'
+print(a)  # print the outpu 
+b = True # Assign the Boolean value to 'b'
+c = [1, 2, 3, 4] # Assign the list value to 'c'
+
+# Simple if-else
+if b: # Checking if b==True is or not? 
+    print(c) # Python use identation instead of '{}'
+else:
+    print(z)
+
+"""
+It is for multi-lines comments
+"""
+
+"""
+if you write Python witout identation you will get Error
+
+if b:
+print(c)
+else c:
+print(z)
+
+"""
