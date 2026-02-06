3ebb35430089f530a08e6b43e71ccf0498717ce4
Nilesh Medda <meddanilesh7@gmail.com>
2026-02-06 23:32:27 +0530

    Add 02_strings.py file

---
+++ codes/02_data_types/02_strings.py
@@ -0,0 +1,20 @@
+a='hello'
+b="coders"
+
+print(type(a), type(b))
+print(a+b)
+print(a*3)
+print(b[1])
+print(b[-1])
+print(b[1:4])
+print(b[3:])
+print(b[:3])
+print(b[-2:])
+print(b[2:]+b[:2])
+print(b[4:]+b[:4])
+# print(b[42])
+print(b[3:42])
+print(b[42:])
+print('J'+b[1:2]+'k'+b[3:])
+c='qwertyuiopasdfghjklzxcvbnm'
+print(len(c))
