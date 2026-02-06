23f918b6f4e297b66b9796897819f4376cdd6347
Nilesh Medda <meddanilesh7@gmail.com>
2026-02-06 23:31:45 +0530

    Modify

--- codes/02_data_types/01_numbers.py
+++ codes/02_data_types/01_numbers.py
@@ -4,12 +4,4 @@ a = 10 # data type - Integer
 b = 3.14 # data type - Floating point
 c = 2 + 3j # data type - Complex
 
-# Operators in Python are + - * / // % **
-
-print(a + b) # Addition
-print(a - b) # Subtraction
-print(a * b) # Multiplication
-print(a / b) # Division
-print(a // b) # Quotient
-print(a % b) # Remainder
-print(a ** b) # Power
+print(type(a), type(b), type(c))
