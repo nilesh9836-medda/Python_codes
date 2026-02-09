3c14456ccbd2b54aa4403a189ce9bbd254d4600a
Nilesh Medda <meddanilesh7@gmail.com>
2026-02-09 21:52:29 +0530

    Add 07_membership.py

---
+++ codes/04_operators/07_membership.py
@@ -0,0 +1,13 @@
+x = 24
+y = 20
+lis = [10, 20, 30, 40, 50]
+
+if x not in lis:
+    print("x is NOT present in given list")
+else:
+    print("x is present in given list")
+
+if y in lis:
+    print("y is present in given list")
+else:
+    print("y is NOT present in given list")
