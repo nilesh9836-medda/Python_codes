c68640cf7d105b3e17210e1b19519879543b9452
Nilesh Medda <meddanilesh7@gmail.com>
2026-02-09 16:02:45 +0530

    Add 01_output.py

---
+++ codes/03_IO/01_output.py
@@ -0,0 +1,10 @@
+a=5
+b=10
+print("a=",a," and b=",b)
+print("a = {0} and b = {1}".format(a,b))
+print("a=%d and b=%d"%(a,b))
+
+print('{0:<2} | {1:>2}'.format('a','b'))
+print('{0:<2} | {1:>2}'.format(a,b))
+
+print(f"a={a} and b={b}")
