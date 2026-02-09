166a148d6af4536dea385f1ddbbd5c4ca3c0858c
Nilesh Medda <meddanilesh7@gmail.com>
2026-02-09 19:33:21 +0530

    Add 02_input.py file

---
+++ codes/03_IO/02_input.py
@@ -0,0 +1,8 @@
+a = input("Enter something: ")      # take input as string
+print(f"\nYou have entered...\n{a}")
+
+b = int(input("Enter a number: "))    # take input as integer value
+print(f"\nYou have entered...\n{b}")
+
+c = float(input("Enter a number: "))    # take input as floating point value
+print(f"\nYou have entered...\n{c}")
