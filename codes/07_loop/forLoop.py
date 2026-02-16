00dfa6145d67d1cc77c8d7456bf706f148bcd84c
Nilesh Medda <meddanilesh7@gmail.com>
2026-02-17 04:36:35 +0530

    update

--- codes/07_loop/forLoop.py
+++ codes/07_loop/forLoop.py
@@ -1,8 +1,17 @@
 for i in [1,'a',2,'b',3,'c',4,'d',5,'e']:
     print(i,end="")
 print()
+
 for i in range(10):
     print(i,end=" ")
 print()
+
 for i in range(1,10):
     print(2**i,end=" ")
+print()
+
+a = [i for i in "Python"]
+print(a)
+
+a = [i**3 for i in range(1,11,+2)]
+print(a)
