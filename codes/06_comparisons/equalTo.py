ab7ae5508f2d7fb13b8e7593111c113e0158f573
Nilesh Medda <meddanilesh7@gmail.com>
2026-02-16 14:25:18 +0530

    Add Equal to and Not Equal To

---
+++ codes/06_comparisons/equalTo.py
@@ -0,0 +1,18 @@
+# '==' -> Equal To
+
+print(
+    12 == 12,   # True
+    12 == 1,    # False
+    '12' == '12',   # True
+    'spam' == 'spam',   # True
+    'spam' == 'spam ',  # False
+    '12' == 12  # False
+)
+
+# '!=' -> Not Equal To
+
+print(
+    12 != 1,    # True
+    12 != '12', # True
+    '12' != '12'    # False
+)
