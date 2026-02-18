264a1476ca64617abfe0a82ac7caa63b950bbc58
Nilesh Medda <meddanilesh7@gmail.com>
2026-02-18 13:48:33 +0530

    Add creating list

---
+++ codes/08_list/creating.py
@@ -0,0 +1,12 @@
+c = []
+b = list()
+a = [0,1,2,3,4,4]
+print(type(c),type(b),type(a))
+b = list("abc")
+print(b)
+e = [0] * 5
+print(e)
+a[5] = 5
+print(a)
+b = ['a', 'b', input("Enter: ")]
+print(b)
